a5d0908b041b8ecac75e3e172498dea19945b67a
DakotaPatterson <158085432+DakotaPatterson@users.noreply.github.com>
2024-07-02 14:22:59 -0400

    Added the loginform.jsx updates and made sure an error message was created

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -1,14 +1,18 @@
 // see SignupForm.js for comments
 import { useState } from 'react';
+// Important for useMutation: We import the useMutation hook from @apollo/client
+import { useMutation } from '@apollo/client';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { loginUser } from '../utils/API';
+// Important for useMutation: We import the specific query we'd like to perform from the mutations.js utility
+import { LOGIN_USER  } from '../../utils/mutations';
 import Auth from '../utils/auth';
 
 const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
+  const [loginUser, { error }] = useMutation(LOGIN_USER);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -26,15 +30,11 @@ const LoginForm = () => {
     }
 
     try {
-      const response = await loginUser(userFormData);
+      const { data } = await loginUser({
+        variables: { ...userFormData },
+      });
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
+      Auth.login(data.login.token);
     } catch (err) {
       console.error(err);
       setShowAlert(true);
@@ -85,6 +85,7 @@ const LoginForm = () => {
           Submit
         </Button>
       </Form>
+      {error && <Alert variant='danger'>Login failed</Alert>}
     </>
   );
 };
