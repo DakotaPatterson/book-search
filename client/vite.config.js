f7d1b962c239cac49d53f4151de2204faa3d578e
DakotaPatterson <158085432+DakotaPatterson@users.noreply.github.com>
2024-07-18 11:18:01 -0400

    Added vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,17 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist'
+  },
   server: {
     port: 3000,
     open: true,
     proxy: {
-      '/api': {
+      '/graphql': {
         target: 'http://localhost:3001',
+        changeOrigin: true,
         secure: false,
-        changeOrigin: true
       }
     }
   }
