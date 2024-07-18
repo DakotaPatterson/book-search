e9ac84daf02a4731bdf5626cc024b6d6c7ff9128
DakotaPatterson <158085432+DakotaPatterson@users.noreply.github.com>
2024-07-18 13:05:59 -0400

    Added new key and fixed other issues!

--- client/src/pages/SavedBooks.jsx
+++ client/src/pages/SavedBooks.jsx
@@ -62,8 +62,8 @@ const SavedBooks = () => {
         </h2>
         <Row>
           {userData.savedBooks.map((book) => {
-              <Col md="4">
-                <Card key={book.bookId} border='dark'>
+              <Col md="4" key={book.bookId}>
+                <Card border='dark'>
                   {book.image ? <Card.Img src={book.image} alt={`The cover for ${book.title}`} variant='top' /> : null}
                   <Card.Body>
                     <Card.Title>{book.title}</Card.Title>
